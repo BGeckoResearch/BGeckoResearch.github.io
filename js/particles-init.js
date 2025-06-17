9451133f6f710fe4fbd9d50218d17e328ff2fdec
Christopher Lee Burgess <45471705+BGeckoResearch@users.noreply.github.com>
2025-06-17 15:13:29 -0500

    Create particles-init.js

---
+++ js/particles-init.js
@@ -0,0 +1,8 @@
+// particles-init.js
+particlesJS.load(
+    'particles-js',        // ID of the div
+    'js/particles.json',   // Path to your JSON config file
+    function () {
+        console.log('particles.js config loaded');
+    }
+);
